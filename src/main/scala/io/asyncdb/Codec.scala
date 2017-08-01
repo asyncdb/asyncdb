fa12f631da249a2f4eeb86f82bd554413e74957c
jilen <jilen.zhang@gmail.com>
2017-08-01 09:00:35 +0800

    fix

--- src/main/scala/io/asyncdb/Codec.scala
+++ src/main/scala/io/asyncdb/Codec.scala
@@ -1,1 +1,8 @@
 package io.asyncdb
+
+trait BasicEncoders {
+  val byteEncoder: Encoder[Byte]
+  val intEncoder: Encoder[Int]
+  val longEncoder: Encoder[Long]
+  val bytesEncoder: Encoder[Array[Byte]]
+}
