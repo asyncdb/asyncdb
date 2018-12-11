7f8f02dd1b7682b05a0f2ef4d077196cef6ab2d2
jilen <jilen.zhang@gmail.com>
2018-12-11 18:22:27 +0800

    fix

--- src/main/scala/io/asyncdb/nio/mysql/HandshakeInit.scala
+++ src/main/scala/io/asyncdb/nio/mysql/HandshakeInit.scala
@@ -72,8 +72,6 @@ object HandshakeInit {
       b <- basicHandshakeDataReader
       e <- extraHandshakeDataReader
     } yield {
-      println(b)
-      println(e)
       val c = CharsetMap.of(e.characterSet.value)
       HandshakeInit(
         protocol = b.protocol.value,
