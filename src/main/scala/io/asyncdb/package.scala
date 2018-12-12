11dccb48da387dbbe18e5b4cdffa462d97ed1151
yison <yison@dripower.com>
2018-12-12 15:04:06 +0800

    Add ConnA via Free

--- src/main/scala/io/asyncdb/package.scala
+++ src/main/scala/io/asyncdb/package.scala
@@ -4,6 +4,8 @@ import java.nio.ByteBuffer
 import java.nio.channels.AsynchronousSocketChannel
 
 package object asyncdb {
-  private[asyncdb] type Buf = ByteBuffer
-  private[asyncdb] type ASC = AsynchronousSocketChannel
+  private[asyncdb] type Buf    = ByteBuffer
+  private[asyncdb] type ASC    = AsynchronousSocketChannel
+  private[asyncdb] type Data   = Any
+  private[asyncdb] type Result = Any
 }
